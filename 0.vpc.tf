e0979ae94613df36850b31487917014d8caf94b4
eandreg <andre_new_york@yahoo.com>
2025-05-05 08:28:00 -0400

    add .gitignore

---
+++ 0.vpc.tf
@@ -0,0 +1,27 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
+resource "google_compute_network" "superman" {
+  name                            = "superman"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+resource "google_compute_network" "batman" {
+  name                            = "batman"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+resource "google_compute_network" "robin" {
+  name                            = "robin"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
